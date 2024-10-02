5fc08d35b700eb01bed605322731dcc6559efa6f
Thom-ti <ti.thomsan@gmail.com>
2024-10-02 17:21:07 +0700

    navbar & left sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,68 @@
-import React from "react";
+import { ClockAlert, Handshake, HomeIcon, Newspaper, Store, TvMinimalPlay, Users } from "lucide-react";
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">CC18 Fakebook</h1>
-      <div className="btn btn-primary">OK</div>
-    </>
+    <div className="min-h-screen bg-yellow-100">
+      <Header />
+      <main className="relative flex bg-gray-100 border pt-14">
+        {/* Left */}
+        <div className="fixed top-14 h-full w-[350px] overflow-auto flex flex-col gap-2 min-w-[220px] max-xl:w-[200px]">
+          <button className="btn border-none shadow-none justify-start gap-x-2 hover:bg-opacity-20">
+            <HomeIcon className="w-8" />
+            Andy Codecamp
+          </button>
+          <button className="btn border-none shadow-none justify-start gap-x-2 hover:bg-opacity-20">
+            <Users className="w-8" />
+            Friends
+          </button>
+          <button className="btn border-none shadow-none justify-start gap-x-2 hover:bg-opacity-20">
+            <Handshake className="w-8" />
+            Groups
+          </button>
+          <button className="btn border-none shadow-none justify-start gap-x-2 hover:bg-opacity-20">
+            <ClockAlert className="w-8" />
+            Memories
+          </button>
+          <button className="btn border-none shadow-none justify-start gap-x-2 hover:bg-opacity-20">
+            <TvMinimalPlay className="w-8" />
+            Videos
+          </button>
+          <button className="btn border-none shadow-none justify-start gap-x-2 hover:bg-opacity-20">
+            <Store className="w-8" />
+            Marketplace
+          </button>
+          <button className="btn border-none shadow-none justify-start gap-x-2 hover:bg-opacity-20">
+            <Newspaper className="w-8" />
+            Feed
+          </button>
+        </div>
+        {/* Center */}
+        <div className="w-[680px] mx-auto min-h-screen my-3 flex flex-col gap-4 rounded-lg">
+          <div className="artboard phone-3">414x736</div>
+          <div className="artboard phone-3">414x736</div>
+          <div className="artboard phone-3">414x736</div>
+          <div className="artboard phone-3">414x736</div>
+        </div>
+        {/* Right */}
+        <div className="fixed top-14 right-0 h-full w-[350px] overflow-auto flex flex-col gap-2 pt-4 px-2 max-xl:hidden">
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+        </div>
+      </main>
+    </div>
   );
 };
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,83 @@
+import {
+  Bell,
+  FacebookIcon,
+  Handshake,
+  HomeIcon,
+  Menu,
+  MessageCircle,
+  Search,
+  Store,
+  TvMinimalPlay,
+} from "lucide-react";
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="h-14 w-full fixed top-0 z-10 px-1 flex justify-between shadow-lg bg-white">
+      {/* Logo + input */}
+      <div className="flex gap-2 items-center flex-1">
+        <FacebookIcon
+          size={24}
+          color="blue"
+          className="w-12 h-12 p-1 border-2 border-blue-700 rounded-full hover:cursor-pointer"
+        />
+        <label className="input input-bordered flex items-center gap-2 w-64 h-10 rounded-full">
+          <input type="text" className="grow" placeholder="Search" />
+          <Search color="gray" />
+        </label>
+      </div>
+      {/* Center group-icons */}
+      <div className="flex gap-2 flex-1 justify-center items-center">
+        <div className="flex justify-center w-20 hover:cursor-pointer hover:border-b-2 hover:border-blue-900">
+          <HomeIcon size={36} color="gray" className="w-2/5" />
+        </div>
+        <div className="flex justify-center w-20 hover:cursor-pointer hover:border-b-2 hover:border-blue-900">
+          <TvMinimalPlay size={36} color="gray" className="w-2/5" />
+        </div>
+        <div className="flex justify-center w-20 hover:cursor-pointer hover:border-b-2 hover:border-blue-900">
+          <Store size={36} color="gray" className="w-2/5" />
+        </div>
+        <div className="flex justify-center w-20 hover:cursor-pointer hover:border-b-2 hover:border-blue-900">
+          <Handshake size={36} color="gray" className="w-2/5" />
+        </div>
+      </div>
+      {/* Right menu */}
+      <div className="flex gap-3 flex-1 justify-end">
+        <div className="avatar justify-center items-center">
+          <div className="w-10 h-10 rounded-full !flex justify-center items-center bg-gray-300 hover:bg-gray-400">
+            <Menu className="w-5" />
+          </div>
+        </div>
+        <div className="avatar justify-center items-center">
+          <div className="w-10 h-10 rounded-full !flex justify-center items-center bg-gray-300 hover:bg-gray-400">
+            <MessageCircle className="w-5" />
+          </div>
+        </div>
+        <div className="avatar justify-center items-center">
+          <div className="w-10 h-10 rounded-full !flex justify-center items-center bg-gray-300 hover:bg-gray-400">
+            <Bell className="w-5" />
+          </div>
+        </div>
+        <div className="dropdown dropdown-end mt-2">
+          <div tabIndex={0} role="button" className="">
+            <div className="avatar">
+              <div className="w-10 h-10 rounded-full">
+                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              </div>
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+          >
+            <li onClick={() => alert(69)}>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
